9cfc70a4efa680ae6658fb1f5f96ebd05fb95ea4
hiramramirez <hiram.j.ramirez@gmail.com>
2022-08-30 17:28:17 -0500

    Completed column added to assignments, flashcards and tasks, and therapist column in user edited to default false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_29_231959) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_222506) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_231959) do
     t.bigint "task_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
     t.index ["task_id"], name: "index_assignments_on_task_id"
     t.index ["user_id"], name: "index_assignments_on_user_id"
   end
@@ -39,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_231959) do
     t.bigint "task_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
     t.index ["task_id"], name: "index_flashcards_on_task_id"
   end
 
@@ -47,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_231959) do
     t.string "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -59,7 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_29_231959) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.boolean "therapist"
+    t.boolean "therapist", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
