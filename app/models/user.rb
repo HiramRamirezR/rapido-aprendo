f4292a646f0063f75a57e0a3e266ade35649e3e1
Daniel Santillan <ldms99@hotmail.com>
2022-08-31 15:55:50 -0500

    avatar code is on the navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,17 @@ class User < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :tasks, through: :assignments
   has_many :flashcards, through: :answers
+  has_one_attached :photo
 
   validates :first_name, :last_name, presence: true
+
+  private
+
+  def default_avatar
+    unless self.photo.attached?
+      file = URI.open("https://res.cloudinary.com/dqfnzfthu/image/upload/v1661464299/avatar-g0212f9dd5_640_dsnzfw.png")
+      self.photo.attach(io: file, filename: "default_avatar_#{self.first_name}", content_type: "image/png")
+      self.save
+    end
+  end
 end
