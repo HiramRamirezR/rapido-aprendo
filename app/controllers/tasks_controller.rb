7c79e2064f75de1e567c291490f6803d719103ee
hiramramirez <hiram.j.ramirez@gmail.com>
2022-08-31 13:01:05 -0500

    development, production and storage, and task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,15 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
