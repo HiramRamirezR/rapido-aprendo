09f7bfcc14eb96f50661b940c6d15f7acd6926a3
hiramramirez <hiram.j.ramirez@gmail.com>
2022-09-09 12:59:47 -0500

    flashcards instructions and image column added in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_08_194139) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_09_174738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_08_194139) do
     t.datetime "updated_at", null: false
     t.string "content"
     t.string "answer"
+    t.string "image"
     t.index ["task_id"], name: "index_flashcards_on_task_id"
   end
 
